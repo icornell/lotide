da6a9e18e96b39fc6e72a66bb6fd0078529f0989
India Cornell <indiaccornell@gmail.com>
2023-06-19 16:30:20 -0700

    adding assertion function with emojis

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+//reminder - if nothing happens (i.e. true), all good! otherwise error message in console
+//function implementation:
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+//test code:
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("forty-two", 42);
+assertEqual("towel", "towel");
+assertEqual("Babelfish", "Vogon");
+assertEqual(42, 42);
