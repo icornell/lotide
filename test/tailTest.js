bc01e631e93997180ef53126844fe0d476362fb8
India Cornell <indiaccornell@gmail.com>
2023-07-08 20:13:46 -0700

    refactored tailTest and middleTest with M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns [2,3] for [1,2,3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for ['1']", () => {
+    assert.deepEqual(tail(['1']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
