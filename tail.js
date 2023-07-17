1e77d5aebc0124c890a328ca1f98aa50ca68a01b
India Cornell <indiaccornell@gmail.com>
2023-07-16 17:03:18 -0700

    minor edit for formatting

--- tail.js
+++ tail.js
@@ -1,5 +1,4 @@
 //note: the tail end of the array is all the elements but the "head" or first element.
-const assertEqual = require("./assertEqual"); //importing assertEqual function
 const tail = function (array) {
   let getTailArray = [];
   for (let i = 1; i < array.length; i++) {
